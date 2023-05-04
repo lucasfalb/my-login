eda1c044ba6d942174167994fdfa816b7989eaa0
Lucas F. Albuquerque <90412000+lucasfalb@users.noreply.github.com>
2023-05-04 05:52:24 -0300

    ok

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+import LoginPage from './pages/LoginPage';
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
